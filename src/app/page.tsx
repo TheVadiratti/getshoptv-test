fdaad9f749f98dd1a5a4fd26f8be8d3f8c8861ae
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-20 13:24:30 +0500

    feat: видео на стартовой странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import Styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={Styles.main}>
-      <div />
+      <video className={Styles.video} src="/video/1.mp4" autoPlay loop muted />
     </main>
   );
 }
