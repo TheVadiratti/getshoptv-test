dc87d595923ab3514559e8b4bd2216845a363279
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-21 15:06:46 +0500

    feat: Checkbox компонент

---
+++ src/shared/components/checkbox/checkbox.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { memo, ChangeEventHandler, MouseEventHandler } from 'react';
+import Styles from './checkbox.module.css';
+
+interface Props {
+  isChecked: boolean;
+  htmlFor: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onClick?: MouseEventHandler<HTMLLabelElement>;
+}
+
+const Checkbox = memo(({ isChecked, htmlFor, onChange, onClick }: Props) => (
+  <label
+    className={`${Styles.checkbox} ${isChecked && Styles.checkboxChecked}`}
+    htmlFor={htmlFor}
+    onClick={onClick}
+  >
+    <input
+      className={Styles.inputCheckbox}
+      type="checkbox"
+      onChange={onChange}
+      id={htmlFor}
+      checked={isChecked}
+    />
+  </label>
+));
+
+export default Checkbox;
