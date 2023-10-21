4ddcd59f0b2f7a0fded72ad60826f2073d65a799
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-21 13:33:18 +0500

    fix: убрать лишний код

--- src/features/enter-tel/components/keyboard-num/keyboard-num.tsx
+++ src/features/enter-tel/components/keyboard-num/keyboard-num.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/jsx-no-bind */
 import { memo, useCallback } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
