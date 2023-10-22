1d2a0891d4c63c1d6ff9abfd60e28fb21fc96a22
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-22 13:57:11 +0500

    feat: контекст для навигации в Appointment layout

--- src/app/appointment/layout.tsx
+++ src/app/appointment/layout.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import { ReactNode, useCallback } from 'react';
+import { useState, useCallback, createContext, useMemo } from 'react';
+import type { ReactNode, Dispatch, SetStateAction } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import ButtonClose from '@/shared/components/button-close/button-close';
 import Styles from './layout.module.css';
 
+export const FocusContext = createContext<{
+  currFocus: number,
+  setCurrFocus: Dispatch<SetStateAction<number>>,
+} | null>(null);
+
 function AppointmentLayout({ children }: { children: ReactNode }) {
+  const [currFocus, setCurrFocus] = useState(1);
+
+  const focusState = useMemo(() => ({ currFocus, setCurrFocus }), [currFocus]);
+
   const router = useRouter();
 
   const handleCloseButton = useCallback(() => {
@@ -15,11 +25,14 @@ function AppointmentLayout({ children }: { children: ReactNode }) {
 
   return (
     <main className={Styles.main}>
-      {children}
+      <FocusContext.Provider value={focusState}>
+        {children}
+      </FocusContext.Provider>
       <div className={Styles.overlay}>
         <ButtonClose
           extraClass={Styles.closeButton}
           onClick={handleCloseButton}
+          isFocus={currFocus === 14}
         />
         <div className={Styles.qrCnt}>
           <p className={Styles.hint}>
